f4f57675a60b4bd39bba2f5558d44344dc31a407
Seth <Seth@10.0.0.103>
2018-01-02 16:52:27 -0500

    Taking the archive page, following each link to an APOD post, then geting the link therefrom to the highest rez version; need to take out non-APOD links i.e. youtube, FB, etc.

--- ActualProjects/src/pictureDownloader/APODImageLibrary.java
+++ ActualProjects/src/pictureDownloader/APODImageLibrary.java
@@ -1,8 +1,11 @@
 package pictureDownloader;
 
-import java.io.IOException;
+
 import java.util.List;
-import java.util.regex.Pattern;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,7 +22,7 @@ public class APODImageLibrary {
 	private String URL;
 	
 	public APODImageLibrary(){
-		URL = "https://apod.nasa.gov/apod/astropix.html";
+		URL = "https://apod.nasa.gov/apod/archivepix.html";
 	}
 	
 	public String[] getImageIDs() throws IOException{
@@ -28,15 +31,32 @@ public class APODImageLibrary {
 		Elements listOfPages;
 		String image;
 		
-		System.out.println("-> " + doc.attributes().iterator());
+		Path file = Paths.get("APODArchive.txt");
+//		Just trying to make sure it all looks right, and I find it easier to look at a .txt file than the console. 
+//		Files.newBufferedWriter(file).write(doc.select("a").eachAttr("abs:href").toString());;
+
+		// Acts weird; I can see this flash on the screen, but then the console flips to starting at the body of the page
+		// Possibly just overflowing the size of the console display?
+		System.out.println("The Document as a String: " + doc.toString());
 		
 		String[] aPODImageArray = null;
 		
 		listOfPages = doc.selectFirst("b").children();
 		List<String> listAsList = listOfPages.select("a").eachAttr("abs:href");
-//		for ( int i = 0; i < 5; i++ ){
-//			buildListOfImages (listAsList.get(i).toString());
-//		}
+		
+		// For each image description page, this goes in and gets the link to the high res picture
+		// Working, though it looks to be SUPER slow
+		// Add: put just the names in an array, to compare against owned files.
+		for (String i : listAsList) {
+			String picsDescPage = Jsoup.connect(i).get().select("a").eachAttr("abs:href").get(1);
+			System.out.println(picsDescPage);
+		}
+
+//		Same as above, just looking
+//		Files.newBufferedWriter(file).write(listAsList.toString());
+		
+		
+		
 		
 		try{
 			
--- ActualProjects/src/pictureDownloader/ImageSync.java
+++ ActualProjects/src/pictureDownloader/ImageSync.java
@@ -4,7 +4,8 @@ package pictureDownloader;
  * ImageSync - This is all the GUI stuff & and sync code
  *  + DiskImageLibrary
  *  + APODImageLibrary
- *  - syncLibraries
+ *  - syncLibraries 
+ *  	Maybe create an iterator here which removes matches?
  *  - Whatever GUI stuff you need
  */
 
