f392439f312aad52b794b5911b7b1d8e72b6d628
Warren <warrenmcclure@gmail.com>
2018-01-08 20:48:14 -0500

    new proj: guitarThing

---
+++ ActualProjects/src/guitarThing/Main.java
@@ -0,0 +1,31 @@
+package guitarThing;
+
+/*
+ * 
+ Guitar note finder:
+ Show where else on the fretboard that note lives
+ Show in a different color its compliments?
+ Chords? 
+ 
+***
+Fretboard
+ - frets, each one a half step apart
+String
+ - each with a base vibration rate which, with the fret considered, creates the note
+ - each string's base vibration is the previous one at fret 5, other than B
+
+When a note is indicated, show other places that note appears on the neck
+Also indicate octaves of that note
+***
+
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
